7d9688cb96dd48ba6c0c3dd8983f63357de62067
dkurchigin <kurchigin.dmitry@yandex.ru>
2021-02-21 20:58:23 +0300

    add products

---
+++ src/data/products.js
@@ -0,0 +1,57 @@
+export default [
+  {
+    title: 'Радионяня',
+    price: 3546,
+    img: 'img/radio.jpg'
+  },
+  {
+    title: 'Банка жестяная',
+    price: 3546,
+    img: 'img/!banka.jpg'
+  },
+  {
+    title: 'Дрель советская',
+    price: 3546,
+    img: 'img/!brel.jpg'
+  },
+  {
+    title: 'Расчёска',
+    price: 3546,
+    img: 'img/!ej.jpg'
+  },
+  {
+    title: 'Коробка с дыркой, ЗАЧЕМ?',
+    price: 3546,
+    img: 'img/!fingerbox.jpg'
+  },
+  {
+    title: 'Мыло арестанское',
+    price: 3546,
+    img: 'img/!milo.jpg'
+  },
+  {
+    title: 'Современный ноутбук',
+    price: 3546,
+    img: 'img/!nout.jpg'
+  },
+  {
+    title: 'Итальянская обувь',
+    price: 3546,
+    img: 'img/!obuv.jpg'
+  },
+  {
+    title: 'Сгущенка',
+    price: 3546,
+    img: 'img/!sgu.jpg'
+  },
+  {
+    title: 'Современный смартфон',
+    price: 3546,
+    img: 'img/!tel.jpg'
+  },
+  {
+    title: 'Ёршик Геленджикский',
+    price: 3546,
+    img: 'img/!tual.jpg'
+  },
+]
