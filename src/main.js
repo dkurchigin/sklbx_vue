d4dc39c23807ea4a827468c2fbe24ebe85d6f4b2
dkurchigin <kurchigin.dmitry@yandex.ru>
2021-03-28 08:54:32 +0300

    add 6.2

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import router from '@/router';
 import App from './App.vue';
 import { firstMessage, secondMessage } from './data';
 import alertFunction from './alerts';
+import store from './store';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: (h) => h(App),
 }).$mount('#app');
 
